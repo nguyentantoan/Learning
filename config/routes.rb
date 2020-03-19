dc6955fac7d549d4a716dba1293e864fabc7670b
toan <nguyentantoan999@gmail.com>
2020-03-19 19:40:05 +0700

    signin

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "users/sign_up" => "users#new"
   post "users/sign_up" => "users#create"
+  get "users/sign_in" => "sessions#new"
+  post "users/sign_in" => "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
