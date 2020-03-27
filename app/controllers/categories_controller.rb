aa5019bb9fc6b2a51398986296e3f6e70f4e2ee0
toan <nguyentantoan999@gmail.com>
2020-03-27 17:02:36 +0700

    category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,5 @@
-class Admin::CategoriesController < ApplicationController
+class CategoriesController < ApplicationController
   def index
     @categories = Category.all
-  end
-
-  def new
-  end
-
-  def create
-    @category = Category.new category_params
-
-    if @category.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  # strong params
-  private
-  def category_params
-    params.require(:category).permit :name
   end
 end
