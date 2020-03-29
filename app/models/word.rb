46af0ec26a707cd19abdae1f3cb6e53d47900c18
toan <nguyentantoan999@gmail.com>
2020-03-29 18:13:23 +0700

    init

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@
 
 class Word < ApplicationRecord
   has_many :word_answer,:dependent => :destroy
-  has_many :lesson
+  has_many :lesson, through: :word
   has_many :lesson_word,:dependent => :destroy
 
   accepts_nested_attributes_for :word_answer
