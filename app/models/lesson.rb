e856c4906ea708c26901103a257bf9b6208f1184
toan <nguyentantoan999@gmail.com>
2020-03-30 18:59:30 +0700

    fix model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,8 +2,6 @@
 
 class Lesson < ApplicationRecord
   has_many :lesson_words
-  has_many :word_answers, through: :lesson_words
-  has_many :words, through: :lesson_words
-
+  
   accepts_nested_attributes_for :words
 end
