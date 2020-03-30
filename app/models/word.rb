41568e7ca03567ec9b6c433f611aab9de100ebf0
toan <nguyentantoan999@gmail.com>
2020-03-30 19:07:51 +0700

    fix model1

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Word < ApplicationRecord
-  has_many :word_answers,through: :lesson_words
+  has_many :word_answers
   has_many :lesson_words
 
   accepts_nested_attributes_for :word_answers
