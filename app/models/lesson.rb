020d31a5bc18047c5f244ff2159682ca100dc1b0
toan <nguyentantoan999@gmail.com>
2020-03-30 16:37:51 +0700

    doi sqlsangPg

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,7 @@
 
 class Lesson < ApplicationRecord
   has_many :lesson_words, :dependent => :destroy
-  has_many :words
+  has_many :words, through: :lesson_words
 
-  accepts_nested_attributes_for :word
+  accepts_nested_attributes_for :words
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Word < ApplicationRecord
-  has_many :word_answer,:dependent => :destroy
-  has_many :lesson, through: :word
-  has_many :lesson_word,:dependent => :destroy
+  has_many :word_answers,:through => :lesson_word
+  has_many :lessons, through: :lesson_words
+  has_many :lesson_words,:dependent => :destroy
 
-  accepts_nested_attributes_for :word_answer
+  accepts_nested_attributes_for :word_answers
 end
