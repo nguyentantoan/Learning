dae78f0884e9862de99ea19c5f7917c9c28ae83e
toan <nguyentantoan999@gmail.com>
2020-04-02 17:34:59 +0700

    create word

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Word < ApplicationRecord
-  has_many :word_answers
+  has_many :word_answers, :dependent => :destroy
   has_many :lesson_words
 
   accepts_nested_attributes_for :word_answers
